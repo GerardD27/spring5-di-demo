d2257cf4023fa42a9e23d6491cd32b2963ef4a79
Gerard <Debold.gerard@gmail.com>
2019-07-23 10:43:45 -0400

    Added @Controller and @Autowired to each controller class

--- src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/controllers/ConstructorInjectedController.java
@@ -2,12 +2,17 @@ package guru.springframework.controllers;
 
 import guru.springframework.services.GreetingService;
 import guru.springframework.services.GreetingServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 5/24/17.
  */
+
+@Controller
 public class ConstructorInjectedController {
 
+    @Autowired
     private GreetingService greetingService;
 
     public ConstructorInjectedController(GreetingService greetingService) {
